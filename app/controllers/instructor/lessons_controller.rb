38a3527499d0cca4affbd064ed24f51c7434ea2e
martin mcreynolds <martinmcrey@gmail.com>
2019-08-03 10:53:13 -0500

    lesson modal not working

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,8 +1,7 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:create]
   before_action :require_authorized_for_current_lesson, only: [:update]
-
 
 
   def create
@@ -14,7 +13,7 @@ class Instructor::LessonsController < ApplicationController
     current_lesson.update_attributes(lesson_params)
     render plain: 'updated!'
   end
-
+    
   private
 
   def require_authorized_for_current_lesson
@@ -29,16 +28,18 @@ class Instructor::LessonsController < ApplicationController
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
+      return render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
+    
   helper_method :current_section
   def current_section
     @current_section ||= Section.find(params[:section_id])
   end
 
+
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
-  end
+  end  
 end
